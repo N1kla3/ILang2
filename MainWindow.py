b66545c0735cbba8560f4ba302405823dddb6da8
kolya <kolya.vladimirsky@gmail.com>
2021-11-18 00:02:22 +0300

    ngrams working

--- MainWindow.py
+++ MainWindow.py
@@ -5,8 +5,12 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from nltk import ngrams
 
-urllist = {"",
-           ""}
+urllist = {"https://en.wikipedia.org/wiki/Dog": "ENG",
+           "https://en.wikipedia.org/wiki/Dog": "ENG",
+           "https://en.wikipedia.org/wiki/Dog": "ENG",
+           "https://en.wikipedia.org/wiki/Dog": "ENG",
+           "https://en.wikipedia.org/wiki/Dog": "ENG",
+           "https://es.wikipedia.org/wiki/Canis_familiaris": "ESP"}
 
 
 class MainWindow(QMainWindow):
@@ -15,11 +19,9 @@ class MainWindow(QMainWindow):
         uic.loadUi('gui.ui', self)
         self.btn_search.clicked.connect(self.btn_search_clicked)
         self.text_ngrams_map = []
-        self.read_test_examples()
+        self.data_list = []
+        self.create_data_dictionary()
         nltk.download('punkt')
-
-    def read_test_examples(self):
-        pass
 
     def btn_search_clicked(self):
         result = ""
@@ -28,17 +30,42 @@ class MainWindow(QMainWindow):
         soup = BeautifulSoup(html)
         raw = soup.get_text()
         temp_map = self.calculate_ngrams(raw)
-        print(raw)
+        res = self.detect_language(temp_map)
+        print(res)
 
     def calculate_ngrams(self, text: str):
         result = {}
         for token in nltk.sent_tokenize(text):
             for i in range(0, 4):
-                for grams in ngrams(token.split(), i):
+                for grams in ngrams(token.lower().split(), i):
                     if result.get(" ".join(grams)):
                         result[" ".join(grams)] += 1
                     else:
                         result[" ".join(grams)] = 1
-                    print(grams)
         result = sorted(result.items(), key=lambda x: x[1], reverse=True)
         return result
+
+    def create_data_dictionary(self):
+        for url in urllist:
+            html = urlopen(url).read()
+            soup = BeautifulSoup(html)
+            raw = soup.get_text()
+            temp_map = self.calculate_ngrams(raw)
+            temp_list = [(t[0]) for t in temp_map]
+            self.data_list.append((urllist[url], temp_list))
+
+    def detect_language(self, ngram_list_amount):
+        result = [1111111111, "url", "lang"]
+        for example in self.data_list:
+            value = 0
+            for ngram in list(ngram_list_amount)[:100]:
+                local_pos = ngram_list_amount.index(ngram)
+                if ngram[0] in example[1]:
+                    db_pos = example[1].index(ngram[0])
+                    diff = abs(local_pos - db_pos)
+                    value += diff
+            if result[0] > value:
+                result[0] = value
+                result[2] = example[0]
+        return result
+# TODO: add urlist and all comparable urls
