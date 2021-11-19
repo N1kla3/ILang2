252096a443885e95fbb31ce75cebeb9cb53f7cfb
kolya <kolya.vladimirsky@gmail.com>
2021-11-19 03:17:09 +0300

    ngrams working

--- MainWindow.py
+++ MainWindow.py
@@ -1,9 +1,11 @@
+
 import nltk
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-from nltk import ngrams
+from nltk import ngrams, RegexpTokenizer
+from regex import split
 
 urllist = {"https://en.wikipedia.org/wiki/Dog": "ENG",
            "https://en.wikipedia.org/wiki/Dog": "ENG",
@@ -29,8 +31,11 @@ class MainWindow(QMainWindow):
         html = urlopen(str).read()
         soup = BeautifulSoup(html)
         raw = soup.get_text()
+        alpha_map = self.calculate_alpha(raw)
         temp_map = self.calculate_ngrams(raw)
         res = self.detect_language(temp_map)
+        result = self.detect_lang_alpha(alpha_map)
+        print(result)
         print(res)
 
     def calculate_ngrams(self, text: str):
@@ -45,14 +50,30 @@ class MainWindow(QMainWindow):
         result = sorted(result.items(), key=lambda x: x[1], reverse=True)
         return result
 
+    def calculate_alpha(self, text: str):
+        tokenizer = RegexpTokenizer(r'\w+')
+        words = tokenizer.tokenize(text)
+        letters = {}
+        for word in words:
+            for letter in list(word.lower()):
+                if letters.get(letter):
+                    letters[letter] += 1
+                else:
+                    letters[letter] = 1
+        result = sorted(letters.items(), key=lambda x: x[1], reverse=True)
+        print(result)
+        return result
+
     def create_data_dictionary(self):
         for url in urllist:
             html = urlopen(url).read()
             soup = BeautifulSoup(html)
             raw = soup.get_text()
             temp_map = self.calculate_ngrams(raw)
+            alpha_map = self.calculate_alpha(raw)
             temp_list = [(t[0]) for t in temp_map]
-            self.data_list.append((urllist[url], temp_list))
+            alpha_list = [(t[0]) for t in alpha_map]
+            self.data_list.append((urllist[url], temp_list, alpha_list))
 
     def detect_language(self, ngram_list_amount):
         result = [1111111111, "url", "lang"]
@@ -62,6 +83,21 @@ class MainWindow(QMainWindow):
                 local_pos = ngram_list_amount.index(ngram)
                 if ngram[0] in example[1]:
                     db_pos = example[1].index(ngram[0])
+                    diff = abs(local_pos - db_pos)
+                    value += diff
+            if result[0] > value:
+                result[0] = value
+                result[2] = example[0]
+        return result
+
+    def detect_lang_alpha(self, alpha_list):
+        result = [1111111111, "url", "lang"]
+        for example in self.data_list:
+            value = 0
+            for letter in alpha_list:
+                local_pos = alpha_list.index(letter)
+                if letter[0] in example[2]:
+                    db_pos = example[2].index(letter[0])
                     diff = abs(local_pos - db_pos)
                     value += diff
             if result[0] > value:
